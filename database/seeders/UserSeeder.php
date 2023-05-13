fdfb1965a7c26c35f4d802e709f857a5ff6cc5b2
Javi <javirodriguezbnk@gmail.com>
2023-05-13 13:17:42 +0200

    initial commit

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Seeders;
+
+use Hash;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Str;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        DB::table('users')->insert([
+            'name' => 'Javi',
+            'email' => 'javirodriguezbnk@gmail.com',
+            'password' => Hash::make('28febrero'),
+        ]);
+    }
+}
---
+++ lang/es/passwords.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Password Reset Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are the default lines which match reasons
+    | that are given by the password broker for a password update attempt
+    | has failed, such as for an invalid token or invalid new password.
+    |
+    */
+
+    'reset' => 'Contraseña reseteada.',
+    'sent' => 'Le hemos enviado un mail con un link de reseteo de contraseña.',
+    'throttled' => 'Por favor, espere.',
+    'token' => 'EL token de reseteo de contraseña es inválido.',
+    'user' => "No existen usuarios con esas credenciales.",
+
+];
